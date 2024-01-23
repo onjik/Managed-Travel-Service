bca286bfeaa08b9b1c1ee933565e5ee408429297
Onji Kim <xodks672@gmail.com>
2024-01-23 12:49:04 +0900

    feat : euraka 기반 service discovery server

---
+++ backend/service_discovery/eureka_server/src/test/java/click/porito/eureka_server/ServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package click.porito.eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
